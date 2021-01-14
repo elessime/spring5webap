429d0c5270ace41f8a39c502fc52563a6bdd1a0e
elessime <tseczkowski@gmail.com>
2021-01-14 23:52:35 +0100

    addedHE

--- src/main/java/com/elessime/spring5webap/Author.java
+++ src/main/java/com/elessime/spring5webap/Author.java
@@ -66,6 +66,33 @@ public class Author {
         this.id = id;
     }
 
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + id;
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Author other = (Author) obj;
+        if (id != other.id)
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Author [books=" + books + ", firstName=" + firstName + ", id=" + id + ", lastname=" + lastname + "]";
+    }
+
     
     
 }
